460148b64cdbbde4632dd956e6f574a62477390f
abdalla aboaziz <abdallaaboaziz@gmail.com>
2026-01-24 17:28:17 +0200

    GenericRepo& UnitOf Work pattern

---
+++ ECommerce.Presistance/Repository/GenericRepository.cs
@@ -0,0 +1,35 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities;
+using ECommerce.Presistance.Data.DBContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance.Repository
+{
+    public class GenericRepository<TEnity, TKey> : IGenericRepository<TEnity, TKey> where TEnity : BaseEnitiy<TKey>
+    {
+        private readonly StoreDbContext _dbContext;
+
+        public GenericRepository(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(TEnity entity)=> await _dbContext.Set<TEnity>().AddAsync(entity);
+
+        public async Task<IEnumerable<TEnity>> GetAllAsync() =>await _dbContext.Set<TEnity>().ToListAsync();
+
+
+        public async Task<TEnity?> GetByIdAsync(TKey id) => await _dbContext.Set<TEnity>().FindAsync(id);
+
+
+        public void Remove(TEnity entity) => _dbContext.Set<TEnity>().Remove(entity);
+
+
+        public void Update(TEnity entity) => _dbContext.Set<TEnity>().Update(entity);
+
+    }
+}
---
+++ ECommerce.Presistance/Repository/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities;
+using ECommerce.Presistance.Data.DBContexts;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreDbContext _dbContext;
+        private readonly Dictionary<Type, object> _repository =[];
+        public UnitOfWork(StoreDbContext dbContext)
+        {
+          _dbContext = dbContext;
+        }
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEnitiy<TKey>
+        {
+            var entityType = typeof(TEntity);
+            if (_repository.TryGetValue(entityType, out object? repository))
+            {
+                return (IGenericRepository<TEntity, TKey>)repository;
+            }
+
+            var newRepository = new GenericRepository<TEntity, TKey>(_dbContext);
+            _repository[entityType] = newRepository;
+            return newRepository;
+
+        }
+
+        public async Task<int> SaveChangesAsync()=> await _dbContext.SaveChangesAsync();
+
+    }
+}
